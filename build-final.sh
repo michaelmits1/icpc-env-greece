e1519febdc9a78fe9ec1aae6661fe65b7c96ce56
Vasilis Kokotakis <vasiliskokotakis2004@gmail.com>
2025-04-25 17:46:22 +0300

    Revert "Add source command before running ansible. Staging for adding pip module"
    This reverts commit 1340ee94747b5613c3f8efa103ae20b3c85f2f56.

--- build-final.sh
+++ build-final.sh
@@ -79,7 +79,6 @@ vm ansible_port=$SSHPORT ansible_host=127.0.0.1
 vm
 EOF
 
-source /opt/ansible-venv/bin/activate
 ANSIBLE_HOST_KEY_CHECKING=False time ansible-playbook -i $INVENTORY_FILE  --ssh-extra-args="-o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --diff --become -u imageadmin --private-key $SSH_BUILD_KEY main.yml
 rm -f $INVENTORY_FILE
 
